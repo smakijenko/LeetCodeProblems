256684cbd5881f9055c93984667e735c0487975c
Stanisław Makijenko <makijenko02@gmail.com>
2024-06-15 20:32:40 +0200

    Create ReverseInteger.swift

---
+++ LeetCodeProblems/ReverseInteger.swift
@@ -0,0 +1,19 @@
+class Solution {
+func reverse(_ x: Int) -> Int{
+    var xText = String(x)
+    var newXText = ""
+    for i in xText.reversed(){
+        newXText.append(i)
+    }
+    if newXText.last == "-"{
+        newXText.removeLast()
+        newXText.insert("-", at: newXText.startIndex)
+    }
+    if Int(newXText)! >= -2147483648 && Int(newXText)! <= 2147483647{
+        return Int(newXText)!
+    }
+    else{
+        return 0
+    }
+}
+}
