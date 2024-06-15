5f1447fbcb8470fb7e64075754c8c71fe3639a59
Stanisław Makijenko <makijenko02@gmail.com>
2024-06-15 14:43:30 +0200

    Create SortColors.swift

---
+++ LeetCodeProblems/SortColors.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+func sortColors(_ nums: inout [Int]){
+    for i in 0 ..< nums.count{
+        for j in 1 ..< nums.count - i{
+            if(nums[j-1] > nums[j]){
+                nums.swapAt(j-1, j)
+            }
+        }
+        
+    }
+}
