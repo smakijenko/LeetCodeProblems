e284ead0e52e2f65be34a7cabbaa650dde1a2e9b
Stanisław Makijenko <makijenko02@gmail.com>
2024-06-16 11:42:05 +0200

    Update MedianOfTwoSortedArrays.swift

--- LeetCodeProblems/MedianOfTwoSortedArrays.swift
+++ LeetCodeProblems/MedianOfTwoSortedArrays.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double{
     let arrayUnsorted = nums1 + nums2
-    let arraySorted = quicksort(arrayUnsorted)
+    let arraySorted = arrayUnsorted.sorted()
     if arraySorted.count % 2 == 0{
         let val1 = Double(arraySorted[arraySorted.count / 2 - 1])
         let val2 = Double(arraySorted[arraySorted.count / 2])
@@ -12,31 +12,5 @@ func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double{
         let i = Int((Double(arraySorted.count) / 2.0).rounded())
         let val = Double(arraySorted[i - 1])
         return val
-    }
-    
-    func quicksort(_ arr:[Int]) -> [Int]{
-        var lessVal = [Int]()
-        var equalVal = [Int]()
-        var greaterVal = [Int]()
-        
-        if arr.count > 1{
-            let pivotVal = arr[0]
-            
-            for x in arr{
-                if x < pivotVal{
-                    lessVal.append(x)
-                }
-                if x == pivotVal{
-                    equalVal.append(x)
-                }
-                if x > pivotVal{
-                    greaterVal.append(x)
-                }
-            }
-            return (quicksort(lessVal) + equalVal + quicksort(greaterVal))
-        }
-        else{
-            return arr
-        }
     }
 }
